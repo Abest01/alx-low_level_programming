cdb9cb2e4ca8c699a371c4daa0c0da7f0791155c
Abest01 <adejareabdullahi1@gmail.com>
2023-07-17 21:24:39 +0100

    2

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - entry poin
+*
+*Return: 0 means success else -1
+*/
+int main(void)
+{
+	int n;
+    if 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - entry poin
+*
+*Return: 0 means success else -1
+*/
+int main(void)
+{
+	int a = 97;
+
+	for (; a < 123; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
