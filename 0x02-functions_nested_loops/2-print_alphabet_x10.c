e01aead92e5c6a3d06f064c68cbe5426b1223be7
Abest01 <adejareabdullahi1@gmail.com>
2023-07-19 16:27:01 +0100

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet - entry point
+*/
+
+void print_alphabet_x10(void)
+{
+	int i= 0; 
+	int a;
+
+	for (; i < 10; i++)
+	{
+		a = 97;
+		for (; a <123; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');	
+	}
+	
+	
+}
