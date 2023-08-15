8d48901f9f92d3c284ae220959d458dbe400b7dd
Abest01 <adejareabdullahi1@gmail.com>
2023-08-15 21:40:39 +0100

    0x0D-preprocessor

--- 0x02-functions_nested_loops/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    int last = -1024 % 10;
-    printf("%d\n", last);
-
-    return (0);
-}
---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef _SIZ_
+#define _SIZ_
+#define SIZE 1024
+
+#endif
