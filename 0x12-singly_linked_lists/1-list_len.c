0b571fcc19a62a72d65299512fbc110946e6f119
Abest01 <adejareabdullahi1@gmail.com>
2023-08-25 20:37:40 +0100

    List lenght

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* print_list - Function use to print all element
+* @h: constant string
+* Return: The number of nodes
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	list_t *tmp = (list_t *)h;
+
+	if (!h)
+		return (count);
+	while (tmp)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+	return (count);
+}
