5233575dc76ff9a62de744b5fa470474e42ae05a
Abest01 <adejareabdullahi1@gmail.com>
2023-10-03 20:57:18 +0100

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,8 +10,10 @@ char *str_concat(char *s1, char *s2)
 	char *newnode;
 	int i, j, k, l = 0;
 
-	if (!(s1 || s2))
-		return (" ");
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
