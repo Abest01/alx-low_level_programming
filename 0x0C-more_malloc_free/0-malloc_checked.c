f1234b0ddfc051aaba2383efe0a48f9b2308e690
Abest01 <adejareabdullahi1@gmail.com>
2023-10-09 00:08:18 +0100

    all

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,18 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* malloc_checked - Function that allocates memory
-* @b: usigned integer
-* Return: NuLL if malloc fails
-*/
+ * malloc_checked - allocates memory using malloc
+ * @b: one non-negative integer parameter
+ * Return: 98 if malloc_checked failed
+ */
 
 void *malloc_checked(unsigned int b)
 {
-	char *newnode;
+	char *mem;
 
-	newnode = malloc(b);
-	if (!newnode)
+	mem = malloc(b);
+	if (mem == NULL)
 		exit(98);
-	return (newnode);
+	return (mem);
 }
