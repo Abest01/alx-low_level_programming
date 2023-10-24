4c7b7bd2763ce4712395f7251b0ff6b30172177a
Abest01 <adejareabdullahi1@gmail.com>
2023-10-24 08:38:38 +0100

    4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,14 +1,18 @@
 #include "lists.h"
 
 /**
-* free_listint - Function that frees a list
-* @head: The first adress
-*/
+ * free_listint - functin that fress a listint_t list
+ * @head: pointer or head
+ *
+ * Return: Nothing
+ */
 void free_listint(listint_t *head)
 {
+	listint_t *new;
 
-	while (head)
+	while ((new = head) != NULL)
+	{
 		head = head->next;
-	free(head);
-
+		free(new);
+	}
 }
